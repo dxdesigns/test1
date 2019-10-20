f3306663bbd6c9d2cc7e9afab61b28caf0a70101
dxdesigns <55350517+dxdesigns@users.noreply.github.com>
2019-10-20 16:05:24 -0500

    Revert "Fix leaderboard"

--- PagesController.php
+++ PagesController.php
@@ -1,19 +1,11 @@
 <?php 
 
-class Leaderboard{
-
-	public $newString = '';
-	public function __construct(){
-		$this->newString = "This will show new string";
-	}
-	/**
-	 * Leaderboards
-	 */
-	public function leaderboard($data){
-		return view('leaderboard')->with([
-			'data' => User::all(),
-			'newString' => $this->newString,
-		]);
-	}
+/**
+ * Leaderboards
+ */
+public function leaderboard($data){
+	return view('leaderboard')->with([
+		'data' => User::all()
+	]);
 }
 ?>
