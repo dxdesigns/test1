d743aa6cb25bc13fa1f66ae5a330112111cafee4
dxwebmaster <dxwebmaster@gmail.com>
2019-10-20 13:27:36 -0500

    fixed PagesController.php class

--- PagesController.php
+++ PagesController.php
@@ -1,11 +1,19 @@
 <?php 
 
-/**
- * Leaderboards
- */
-public function leaderboard($data){
-	return view('leaderboard')->with([
-		'data' => User::all()
-	]);
+class Leaderboard{
+
+	public $newString = '';
+	public function __construct(){
+		$this->newString = "This will show new string";
+	}
+	/**
+	 * Leaderboards
+	 */
+	public function leaderboard($data){
+		return view('leaderboard')->with([
+			'data' => User::all(),
+			'newString' => $this->newString,
+		]);
+	}
 }
 ?>
